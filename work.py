c3aed1264413ad48eaf8acb50765d0c4447124ea
Aiyna-zahid <aiynazahid625@gmail.com>
2026-07-23 13:51:32 +0500

    Revert "added code in python"

--- work.py
+++
@@ -1,8 +0,0 @@
-def read_html_file(filename):
-    with open(filename, "r") as file:
-        return file.read()
-
-if __name__ == "__main__":
-    content = read_html_file("index.html")
-    print("HTML content loaded:")
-    print(content)
